f2587ac735463c0be9af4b6b55a39818a4b242e9
barbaraserhena <barbara.serhena.mp@rede.ulbra.br>
2026-09-10 19:49:16 -0300

    Create estados.js

---
+++ estados.js
@@ -0,0 +1,9 @@
+export const estado = {
+    tarefas: [],         
+    busca: "",           
+    status: "todos",     
+    prioridade: "todas",
+    ordenacao: "padrao", /
+    carregando: true,
+    erro: null
+};
